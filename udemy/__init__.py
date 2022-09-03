ae477b1044fded0106262c1d55a86d5f7ad99c5a
evil775 <80914593+evil775@users.noreply.github.com>
2022-09-03 14:08:12 +0530

    Update __init__.py

--- udemy/__init__.py
+++ udemy/__init__.py
@@ -1,6 +1,6 @@
 # UdemyBot - A Simple Udemy Free Courses Scrapper
 
-# Copyright (C) 2021-Present EvilerX https://github.com/evil775/Evils_UdemyMinner_bot/
+#  EvilerX https://github.com/evil775/Evils_UdemyMinner_bot/
 
 import os
 
