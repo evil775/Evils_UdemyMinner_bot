23934b77ac6bbf229ec1db6ab8557f52c73cda3d
evil775 <80914593+evil775@users.noreply.github.com>
2022-09-03 14:10:54 +0530

    Update __init__.py

--- udemy/duce/__init__.py
+++ udemy/duce/__init__.py
@@ -1,5 +1,5 @@
 # UdemyBot - A Simple Udemy Free Courses Scrapper
 
-# Copyright (C) 2021-Present Evilerx https://github.com/evil775/Evils_UdemyMinner_bot/
+#  Evilerx https://github.com/evil775/Evils_UdemyMinner_bot/
 
 from udemy.duce.duce import Scrapper
